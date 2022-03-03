5c6b8a6de19cdb14715c2f9bd8715a6b971667df
Arina Perkon <masjandrikovna2.0@rambler.ru>
2022-03-03 16:44:43 +0300

    course api

--- src/api/CoursesAPI.js
+++ src/api/CoursesAPI.js
@@ -1,14 +1,43 @@
 import React, {Component, useState} from 'react';
 import cl from "classnames";
 
+const course = {
+    "name": "Новый курс",
+    "description": "Описание нового курса",
+    "speciality": "string",
+    "professor": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "hours": 0,
+    "contentImage": ""
+};
+
+const updatedCourse = {
+    "id": "6220a96e28160b846e6f3108",
+    "name": "string",
+    "description": "string",
+    "speciality": "string",
+    "professor": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "hours": 0,
+    "contentImage": ""
+};
+
+const module = {
+    "courseId": "6220a96e28160b846e6f3108",
+    "name": "NEW COURSE",
+    "description": "NEW DESCRIPTION",
+    "order": 0
+}
+
 export const APIs = {
     announcement: {},
     course: {
         create: '/api/Course/Create',
-        get: 'api/Course/Get/'
+        get: 'api/Course/Get/',
+        update: 'api/Course/Update',
+        delete: 'api/Course/Delete/'
     },
     сourseCatalog: {
-        get: 'api/CourseCatalog/Get',
+        getAll: 'api/CourseCatalog/GetAll',
+        get: 'api/CourseCatalog/Get?id='
     },
     module: {
         create: '/api/Module/Create',
@@ -36,6 +65,12 @@ export function requestHeader(method, data = {}) {
             },
             body: JSON.stringify(data)
         };
+        case 'DELETE': return {
+            method: method,
+            headers: {
+                accept: '*/*' ,
+            }
+        };
     }
 }
 
@@ -43,8 +78,9 @@ export function sendRequest(url, method, data) {
     console.log(requestHeader(method, data));
     return fetch(url, requestHeader(method, data))
         .then(response => {
-            if (response.status === 200)
+            if (response.status === 200 || response.status === 201) {
                 return response.json();
+            }
             else
                 return { status: response.status, message: response.statusText }
         })
@@ -56,14 +92,34 @@ export function sendRequest(url, method, data) {
         });
 }
 
-export function createCourse(course) {
-    sendRequest(APIs.course.create, 'POST', course);
+export async function createCourse(course) {
+    let response = (await sendRequest(APIs.course.create, 'POST', course)).response;
+    return response;
 }
 
-export function createCourseModules(modules) {
-    modules.forEach(module => {
-        sendRequest(APIs.module.create, 'POST', module);
-    })
+export async function getCourse(id = '6220a96e28160b846e6f3108') {
+    let response = (await sendRequest(APIs.course.get + id, 'GET')).response;
+    return response;
+}
+
+export async function updateCourse(course) {
+    let response = (await sendRequest(APIs.course.update, 'POST', course)).response;
+    return response;
+}
+
+export async function deleteCourse(id = '6220a50228160b846e6f30fe') {
+    let response = (await sendRequest(APIs.course.delete + id, 'DELETE')).response;
+    return response;
+}
+
+export async function getCourseCatalogAll() {
+    let response = (await sendRequest(APIs.сourseCatalog.getAll, 'GET')).response;
+    return response;
+}
+
+export async function getCourseCatalogById(id = '6220a96e28160b846e6f3108') {
+    let response = (await sendRequest(APIs.сourseCatalog.get + id, 'GET')).response;
+    return response;
 }
 
 export async function createModule(courseId, module) {
@@ -72,7 +128,7 @@ export async function createModule(courseId, module) {
 }
 
 export async function getCourseModules(courseId) {
-    let response = (await sendRequest(APIs.course.get + courseId, 'GET')).response;
+    let response = (await getCourse(courseId)).response;
     return response?.modules ? response.modules : [];
 }
 
@@ -91,20 +147,73 @@ class CoursesAPI extends Component {
         super(props);
         this.state = { response: null };
         this.URL = APIs;
+        this.handleCleanState = this.handleCleanState.bind(this);
+        this.handleCourseCreate = this.handleCourseCreate.bind(this);
+        this.handleCourseGet = this.handleCourseGet.bind(this);
+        this.handleCourseUpdate = this.handleCourseUpdate.bind(this);
+        this.handleCourseDelete = this.handleCourseDelete.bind(this);
+        this.handleCourseCatalogGetAll = this.handleCourseCatalogGetAll.bind(this);
+        this.handleCourseCatalogGet = this.handleCourseCatalogGet.bind(this);
     }
 
-    handleClick = (url, method) => {
-        try {
-            sendRequest(url, method);
-        } catch (e) {
-            console.log('ERROR to send request: ', e);
-        }
+    async handleCourseCreate() {
+        let response = await createCourse(course);
+        this.setState({
+            response: JSON.stringify(response)
+        });
+    }
+
+    async handleCourseGet() {
+        let response = await getCourse();
+        this.setState({
+            response: JSON.stringify(response)
+        });
+    }
+
+    async handleCourseUpdate() {
+        let response = await updateCourse(Object.assign(updatedCourse, {description: 'course is up date'}));
+        this.setState({
+            response: JSON.stringify(response)
+        });
+    }
+
+    async handleCourseDelete() {
+        let response = await deleteCourse();
+        this.setState({
+            response: JSON.stringify(response)
+        });
+    }
+
+    async handleCourseCatalogGetAll() {
+        let response = await getCourseCatalogAll();
+        this.setState({
+            response: JSON.stringify(response)
+        });
+    }
+
+    async handleCourseCatalogGet() {
+        let response = await getCourseCatalogById();
+        this.setState({
+            response: JSON.stringify(response)
+        });
+    }
+
+    handleCleanState() {
+        this.setState({
+            response: ''
+        });
     }
 
     render() {
         return (
             <div className={cl('CoursesAPI')}>
-                <button onClick={() => this.handleClick(this.URL.сourseCatalog.get, 'GET')}>CourseCatalog Get</button>
+                <button onClick={this.handleCleanState}>clean</button>
+                <button onClick={this.handleCourseCreate}>course create</button>
+                <button onClick={this.handleCourseGet}>course get</button>
+                <button onClick={this.handleCourseUpdate}>course update</button>
+                <button onClick={this.handleCourseDelete}>course delete</button>
+                <button onClick={this.handleCourseCatalogGetAll}>course catalog get all</button>
+                <button onClick={this.handleCourseCatalogGet}>course catalog get by ID</button>
                 <APIResponse response={this.state.response}/>
             </div>
         );
